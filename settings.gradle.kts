f206899e38239409b9d6b41f8bccec01dbe9d737
soheean1370 <127065983+soheean1370@users.noreply.github.com>
2024-07-17 15:14:11 +0900

    loginDB

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.5.0"
+        id("org.jetbrains.kotlin.android") version "1.9.0"
+        id("com.google.gms.google-services") version "4.4.2"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
