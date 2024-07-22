fed2983aa7dad68bf0411f60d1fe53b2b123b120
Parkhaeil <gp4077@pusan.ac.kr>
2024-07-22 18:12:08 +0900

    한글 안나옴 포함

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,9 +12,12 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.5.0"
+        //alias(libs.plugins.android.application)
         id("org.jetbrains.kotlin.android") version "1.9.0"
+        //alias(libs.plugins.jetbrains.kotlin.android)
         id("com.google.gms.google-services") version "4.4.2"
-        alias(libs.plugins.jetbrains.kotlin.android)
+        //alias(libs.plugins.google.gms.google.services)
+
     }
 }
 dependencyResolutionManagement {
@@ -23,11 +26,13 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
+
+//    versionCatalogs {
+//        create("libs") {
+//            from(files("gradle/libs.versions.toml"))
+//        }
+//    } 7.4이상 버전에서는 추가할 필요가 없다
+
 }
 
 rootProject.name = "My Application"
