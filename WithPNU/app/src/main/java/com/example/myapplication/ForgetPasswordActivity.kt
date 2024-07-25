e00addb5d0ff6a905c07562b8402f6f5ce170817
latteeea <kimtaeran4767@gmail.com>
2024-07-25 18:58:42 +0900

    비번찾기페이지
    비번찾기 페이지 추가]

---
+++ WithPNU/app/src/main/java/com/example/myapplication/ForgetPasswordActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityForgetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.net.PasswordAuthentication
+
+class ForgetPasswordActivity : AppCompatActivity() {
+
+
+}
